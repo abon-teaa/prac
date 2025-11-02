9c4d12c1ae7c58dabc9c1449ca43c2d39610e297
Abonti Jannati <154469170+abon-teaa@users.noreply.github.com>
2025-11-03 00:44:43 +0600

    Time: 11 ms (5.16%) | Memory: 22.5 MB (5.16%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,48 +1,43 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-      if( strs.size()>=1 && strs.size()<=200)
-      {
-        string a="";
-         if(strs.size()==1)
-         { return strs[0];}
-            if(strs[0].length()>=0 && strs[0].length()<=200)
-            {
-                for(int j=0; j< strs[0].length(); j++)
-                {
-                string p= strs[0], q= strs[1];
-                if(p[j]== q[j])
-                {
-                    a+=p[j];
+        if(strs.size()>=1 && strs.size()<=200) {
+            string a="";
+            if(strs.size()==1) { 
+                return strs[0];
+            }
+            if(strs[0].length()>=0 && strs[0].length()<=200) {
+                for(int j=0; j<strs[0].length(); j++) {
+                    string p=strs[0], q=strs[1];
+                    if(p[j]==q[j]) {
+                        a+=p[j];
+                    }
+                    else if(a=="") {
+                        return "";
+                    }
+                    else if(p[j]!=q[j]) {
+                        break;
+                    }
                 }
-                else if( a=="")
-                {
-                    return "";
+                if(strs.size()>1) {
+                    for(int i=2; i<strs.size(); i++) {
+                        if(strs[i].length()>=0 && strs[i].length()<=200) {
+                            for(int k=0; k < a.length() ; k++) {  
+                                string x = strs[i];
+                                if(a.empty()) {
+                                    return "";
+                                }
+                                else if(x[k]!=a[k]) {
+                                    a=a.substr(0, k);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    return a;
                 }
-                else if(p[j]!=q[j])
-                {
-                    break;
-                }
-                }
-                if(strs.size()>1){for(int i=2; i<strs.size();i++)
-                {
-                if(strs[i].length()>=0 && strs[i].length()<=200)
-                
-               {for(int k=0; k < a.length(); k++)
-               {
-                 string x = strs[i];
-                 if(a.empty())
-                 {
-                    return "";
-                 }
-                 else if (x[k]!=a[k])
-                 {
-                    a=a.substr(0, k);
-                    break;
-                 }
-               }} }
-            return a;}
+            }
         }
-      } return ""; 
+        return ""; 
     }
 };
